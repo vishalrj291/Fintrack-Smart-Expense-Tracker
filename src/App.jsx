4c31eeea33569f42814fb8c8e4933c5881a04a03
Vishal Raj <vishalraj27118@gmail.com>
2026-06-04 00:05:50 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import LandingPage from './pages/LandingPage';
 import DashboardPage from './pages/DashboardPage';
 
-function AnimatedRoutes({ theme, setTheme }) {
+export default function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="wait" initial={false}>
       <Routes location={location} key={location.pathname}>
-        <Route
-          path="/"
-          element={<LandingPage theme={theme} setTheme={setTheme} />}
-        />
-        <Route
-          path="/dashboard"
-          element={<DashboardPage theme={theme} setTheme={setTheme} />}
-        />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* Catch-all */}
+        <Route path="*" element={<LandingPage />} />
       </Routes>
     </AnimatePresence>
-  );
-}
-
-export default function App() {
-  const [theme, setTheme] = useState('dark');
-
-  return (
-    <BrowserRouter>
-      <AnimatedRoutes theme={theme} setTheme={setTheme} />
-    </BrowserRouter>
   );
 }
